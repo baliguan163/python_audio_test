168525f5ab0dab89c9c084c2f745e6a500cafd2f
baliguan163 <baliguan163@163.com>
2018-09-07 08:59:52 +0800

    ttsdata测试

--- wavTest2.py
+++ wavTest2.py
@@ -14,8 +14,10 @@ filepath = "E:\\raw\\raw\\tt\\" #添加路径
 filename= os.listdir(filepath) #得到文件夹下的所有文件名称
 for file in filename:
     print('file:' + filepath+file)
-print("get:"+filepath+filename[0])
+print("显示:"+filepath+filename[0])
+#
 
+# filepath = "D:\\work\\adb_sh\\test_encode\\1\\Decode.wav"
 f = wave.open(filepath+filename[0],'rb')
 params = f.getparams()
 nchannels, sampwidth, framerate, nframes = params[:4]
@@ -23,14 +25,19 @@ print("nchannels:",nchannels," ",sampwidth," ",framerate," ",nframes)
 
 
 strData = f.readframes(nframes)#读取音频，字符串格式
+# print(strData)
 waveData = np.fromstring(strData,dtype=np.int16)#将字符串转化为int
+print(waveData)
 waveData = waveData*1.0/(max(abs(waveData)))#wave幅值归一化
+print(waveData)
 
 waveData = np.reshape(waveData,[nframes,nchannels])
+# print(waveData)
 f.close()
 
 # plot the wave
 time = np.arange(0,nframes)*(1.0 / framerate)
+print(time)
 plt.figure()
 
 plt.subplot(5,1,1)
@@ -46,12 +53,10 @@ plt.xlabel("Time(s)")
 plt.ylabel("Amplitude")
 plt.title("Ch-2 wavedata")
 plt.grid('on')#标尺，on：有，off:无。
-
 # plt.subplot(5,1,5)
 # plt.plot(time,waveData[:,2])
 # plt.xlabel("Time(s)")
 # plt.ylabel("Amplitude")
 # plt.title("Ch-3 wavedata")
 # plt.grid('on')#标尺，on：有，off:无。
-
 plt.show()
